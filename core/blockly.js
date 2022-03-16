b9355acb56c9e6a0bf6a49edb5c27f01f712be3b
MohamedTamer94 <Mohamed_Tamer_Ahmed@yahoo.com>
2022-03-16 19:32:12 +0200

    start refactoring class methods

--- core/blockly.js
+++ core/blockly.js
@@ -1316,15 +1316,51 @@ function workspaceToSvg_(workspace, callback) {
  * @param {!Blockly.WorkspaceSvg} workspace The Blockly workspace.
  */
 Blockly.downloadScreenshot = function(workspace) {
-    workspaceToSvg_(workspace, function(datauri) {
-        var a = document.createElement('a');
-        a.download = 'screenshot.png';
-        a.target = '_self';
-        a.href = datauri;
-        document.body.appendChild(a);
-        a.click();
-        a.parentNode.removeChild(a);
-    });
+    var DOMURL = self.URL || self.webkitURL || self;
+
+    function svg(){
+        canvas = Blockly.mainWorkspace.svgBlockCanvas_.cloneNode(true);
+        if (canvas.children[0] === undefined) throw "Couldn't find Blockly canvas."
+
+        canvas.removeAttribute("transform");
+
+        var css = '<defs><style type="text/css" xmlns="http://www.w3.org/1999/xhtml"><![CDATA[' + Blockly.Css.CONTENT.join('') + ']]></style></defs>';
+        var bbox = document.getElementsByClassName("blocklyBlockCanvas")[0].getBBox();
+        var content = new XMLSerializer().serializeToString(canvas);
+
+        xml = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="'
+            + bbox.width + '" height="' + bbox.height + '" viewBox=" ' + bbox.x + ' ' + bbox.y + ' ' + bbox.width + ' ' + bbox.height + '">' +
+            css + '">' + content + '</svg>';
+
+        return new Blob([xml], { type: 'image/svg+xml;base64' });
+    }
+
+    function download(url, filename){
+        let element = document.createElement('a')
+        element.href = url
+        element.download = filename;
+        element.click();
+        DOMURL.revokeObjectURL(element.href)
+    }
+
+    function exportSVG() {
+        download(DOMURL.createObjectURL(svg()),'blocks.svg');
+    }
+
+    function exportPNG(){
+        var img = new Image();
+        img.onload = function() {
+            var canvas = document.createElement('canvas');
+            canvas.width = 800;
+            canvas.height = 600;
+            canvas.getContext("2d").drawImage(img, 0, 0);
+            download(canvas.toDataURL("image/png"),'blocks.png');
+        };
+        img.src = DOMURL.createObjectURL(svg());
+    }
+
+    exportSVG()
+    exportPNG()
 };
 
 // Export symbols that would otherwise be renamed by Closure compiler.
