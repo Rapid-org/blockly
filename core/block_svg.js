d4ba58a94e9037833604efbc673ba3252041a9d5
MohamedTamer94 <mohamed_tamer_ahmed@yahoo.com>
2022-09-16 10:53:12 +0200

    changes

--- core/block_svg.js
+++ core/block_svg.js
@@ -1422,8 +1422,16 @@ Blockly.BlockSvg.prototype.updateColour = function() {
         // Disabled blocks don't have colour.
         return;
     }
+  function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ?
+      [parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)]
+      : null;
+  }
     var hexColour = this.getColour();
-    var rgb = goog.color.hexToRgb(hexColour);
+    var rgb = hexToRgb(hexColour);
     if (this.isShadow()) {
         rgb = goog.color.lighten(rgb, 0.6);
         hexColour = goog.color.rgbArrayToHex(rgb);
